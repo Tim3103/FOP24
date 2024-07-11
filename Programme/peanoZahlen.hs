decc4beb022c0e654f05fa3ac50f855e89772a03
Nils Schäfer <nils.schaefer@tuta.io>
2024-07-11 16:26:59 +0200

    add timesFunction

--- Programme/peanoZahlen.hs
+++ Programme/peanoZahlen.hs
@@ -2,3 +2,6 @@ data N = Z | S N deriving Show
 
 plus :: N -> N -> N
 plus x y = case x of Z -> y ; S x' -> S (plus x' y)
+
+times :: N -> N -> N
+times x y = case x of {Z -> Z; S x' -> plus y (times x' y);}
