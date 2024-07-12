842a844205ee1a7ab37ee265160dcfb5c6229ed1
Nils Schäfer <nils.schaefer@tuta.io>
2024-07-12 13:35:53 +0200

    add min and max to peano

--- Programme/peanoZahlen.hs
+++ Programme/peanoZahlen.hs
@@ -5,3 +5,15 @@ plus x y = case x of Z -> y ; S x' -> S (plus x' y)
 
 times :: N -> N -> N
 times x y = case x of {Z -> Z; S x' -> plus y (times x' y);}
+
+mini :: N -> N -> N
+mini Z Z = Z
+mini Z (S y) = Z
+mini (S x) Z = Z
+mini (S x) (S y) = S (mini x y)
+
+maxi :: N -> N -> N
+maxi Z Z = Z
+maxi Z (S y) = (S y)
+maxi (S x) Z = (S x)
+maxi (S x) (S y) = S (maxi x y)
