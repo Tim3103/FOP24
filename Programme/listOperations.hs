4b858cb1ad48846dc69248c4394c8ce7db3de999
Nils Schäfer <nils.schaefer@tuta.io>
2024-07-13 10:44:58 +0200

    update structure

--- Programme/listOperations.hs
+++
@@ -1,9 +0,0 @@
-data List a = Nil | Cons a (List a) deriving Show
-
-append xs ys = case xs of
-     Nil        -> ys
-     Cons x xs' -> Cons x (append xs' ys)
-
-revers xs = case xs of
-     Nil        -> Nil
-     Cons x xs' -> append (revers xs') (Cons x Nil)
