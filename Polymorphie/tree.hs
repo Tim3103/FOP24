2913abd4e80be83046846e66528efae52df8aadc
Nils Schäfer <nils.schaefer@tuta.io>
2024-07-13 11:42:32 +0200

    add inord

--- Polymorphie/tree.hs
+++ Polymorphie/tree.hs
@@ -24,3 +24,25 @@ pre t = case t of
 -- Cons True (app (Cons False Nil) (Cons False (Cons True Nil))
 -- Cons True (Cons False (app Nil (Cons False (Cons True Nil)))
 -- Cons True (Cons False (Cons False (Cons True Nil)))
+
+inord :: forall a . Bin a -> List a
+inord t = case t of
+  Leaf -> Nil
+  Branch l k r -> app (app (inord l) (Cons k Nil)) (inord r)
+  
+-- Bsp für: inord (Branch (Branch Leaf 2 Leaf) 4 (Branch Leaf 3 (Branch Leaf 1 Leaf)))
+
+-- app (app (inord (Branch Leaf 2 Leaf) (Cons 4 Nil)) (inord (Branch Leaf 3 (Branch Leaf 1 Leaf)))
+-- app (app ((app (app (inord Leaf) (Cons 2 Nil)) (inord Leaf)) (Cons 4 Nil)) (inord (Branch Leaf 3 (Branch Leaf 1 Leaf)))
+-- app (app ((app (app (inord Leaf) (Cons 2 Nil)) (inord Leaf)) (Cons 4 Nil)) (app (app (inord Leaf) (Cons 3 Nil)) (inord ((Branch Leaf 1 Leaf)))))
+-- app (app ((app (app (inord Leaf) (Cons 2 Nil)) (inord Leaf)) (Cons 4 Nil)) (app (app (inord Leaf) (Cons 3 Nil)) (app (app (inord Leaf) (Cons 1 Nil)) (inord Leaf))))
+-- app (app ((app (app Nil (Cons 2 Nil)) Nil) (Cons 4 Nil)) (app (app Nil (Cons 3 Nil)) (app (app Nil (Cons 1 Nil)) Nil)))
+-- app (app ((app (app (Cons 2 Nil)) Nil) (Cons 4 Nil)) (app (app (Cons 3 Nil) (app (Cons 1 Nil) Nil)))
+-- app (app ((app (Cons 2 (app Nil Nil)) Nil) (Cons 4 Nil)) (app (app (Cons 3 Nil) (Cons 1 (app Nil Nil))))
+-- app (app ((app (Cons 2 Nil) Nil) (Cons 4 Nil)) (app (app (Cons 3 Nil) (Cons 1 Nil)))
+-- app (app ((Cons 2 (app Nil Nil)) (Cons 4 Nil)) (app ((Cons 3 (app Nil Nil)) (Cons 1 Nil)))
+-- app (app ((Cons 2 Nil) (Cons 4 Nil)) (app ((Cons 3 Nil) (Cons 1 Nil)))
+-- app ((Cons 2 Nil) (app Nil (Cons 4 Nil))) ((Cons 3 Nil) (app Nil (Cons 1 Nil)))
+-- app ((Cons 2 Nil) (Cons 4 Nil)) ((Cons 3 Nil) (Cons 1 Nil))
+-- Cons 2 (app Nil (Cons 4 Nil) ((Cons 3) (Cons 1 Nil))
+-- Cons 2 ((Cons 4 Nil) ((Cons 3) (Cons 1 Nil)))
